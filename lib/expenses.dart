712ae1d7a80bb8c19f155bf35306c6a0af9acb5d
Sophia Gavrila <sophiagavrila@Sophias-MacBook-Pro.local>
2023-08-23 11:41:26 -0400

    initial commit

---
+++ lib/expenses.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Flutter Course', amount: 10.99, date: DateTime.now(), category: Category.work),
+    Expense(title: 'Cinema', amount: 15.99, date: DateTime.now(), category: Category.leisure),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The Chart'),
+          Expanded(child: 
+            ExpensesList(expenses: _registeredExpenses))
+  
+      ],)
+    );
+  }
+}
---
+++ lib/models/expense.dart
@@ -0,0 +1,26 @@
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+enum Category {
+  food,
+  travel,
+  leisure,
+  work
+}
+
+class Expense {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+  // Initializer Lists are used to initialize final fields that aren't set in the constructor body.
+}
